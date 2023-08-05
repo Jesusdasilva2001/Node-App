19c676b911a09a8eb213758b171a029e60285c97
jesusdasilva2001 <Jesustamba2017@gmail.com>
2023-08-05 21:11:11 +0100

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 //router
-
 app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
-app.listen(3000, () => {
-    console.log(`Node API app is running on port 3000`)
+app.get('/', (req,res) => {
+    res.send('Hello Blog, my name is swap')
+}) 
+
+
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect('mongodb+srv://jesustamba2017:J5XFer2C-wuSR-x@swapadminapi.l9vomkz.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, () => {
+        console.log(`Node API app is running on port 3000`)
+    });
+}).catch((error) => {
+    console.log(error)
 })
