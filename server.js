71bff13f0fcfd7213efd35cb77d5b796917784bc
jesusdasilva2001 <Jesustamba2017@gmail.com>
2023-08-06 01:50:24 +0100

    Update data in Database

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //router
 app.get('/', (req, res) => {
@@ -15,13 +16,64 @@ app.get('/', (req,res) => {
     res.send('Hello Blog, my name is swap')
 }) 
 
+app.get('/products', async(req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} =req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 app.post('/products', async(req, res) => {
     try {
-        const products = await Product.create(req.body)
-        res.status(200).json(products);
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
         
     } catch (error) {
         console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        // we cannot find any product in database
+        if(!product){
+            return res.status(404).json({message: `cannot find my product with ID ${id}`})
+        }
+        const updateProduct = await Product.findById(id);
+        res.status(200).json(updateProduct);
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// delete a product
+
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+
+        }
+        res.status(200).json(product);
+    } catch (error) {
         res.status(500).json({message: error.message})
     }
 })
